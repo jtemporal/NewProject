1f36c8b29c80334f3725c021e9e3d7c3ec0802a0
Jessica Temporal <jessicatemporal@gmail.com>
2015-10-25 15:48:32 -0200

    Created jokenpo.R script

---
+++ Jokenpo/jokenpo.R
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+
+# I prefer to pass my entrys as argments in the command line
+a <- commandArgs(trailingOnly=T)
+
+if(length(a) == 0){
+	entrys <-  c("s,s", "s,p", "s,r", "r,r", "r,p") 
+	# default values in case no argument is passed in command lines
+}else{
+	entrys <- a
+}
+
+jokenpo <- function(entry){
+    win_dict <- c("r","s", "p")
+    names(win_dict) <- c("p", "r", "s")
+
+    for (i in seq_along(entry)){
+    	p1 <- strsplit(entry[i], split=",")[[1]][1] # player 1
+    	p2 <- strsplit(entry[i], split=",")[[1]][2] # player 2
+
+    	if (!(p1 %in% win_dict) || !(p2 %in% win_dict)) print(paste0(entry[i], ' = invalid'))
+    	
+    	else if (win_dict[p1] == p2) print(paste0(entry[i],' = player 1 wins'))
+    	
+    	else if (win_dict[p2] == p1) print(paste0(entry[i],' = player 2 wins'))
+    	
+    	else print(paste0(entry[i],' = tie'))
+    }
+}
+
+jokenpo(entrys)
