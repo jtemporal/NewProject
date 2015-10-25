003c89d0bb200d4b3f5fd8769aed5b62a505b3ac
Jessica Temporal <JTemporal@users.noreply.github.com>
2015-10-25 15:58:07 -0200

    Create README.sh

---
+++ Jokenpo/README.sh
@@ -0,0 +1,9 @@
+# README
+# commands and expected outputs
+chmod +x jokenpo.R
+./jokenpo.R s,s s,p s,r r.r r,r
+#[1] "s,s = tie"
+#[1] "s,p = player 1 wins"
+#[1] "s,r = player 2 wins"
+#[1] "r.r = invalid"
+#[1] "r,r = tie"
