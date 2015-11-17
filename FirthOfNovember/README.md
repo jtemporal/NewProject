39ea487bd277f1c54d1245632b5431905ac5759a
Jessica Temporal <jessicatemporal@gmail.com>
2015-11-17 14:40:17 -0200

    created theFirth

---
+++ FirthOfNovember/README.md
@@ -0,0 +1,3 @@
+# Remember remenber the firth of November
+
+Check the date and print to terminal Guy Fawkes quote.
---
+++ FirthOfNovember/theFirth.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+date <- Sys.Date()
+string <- grep("11-5",date)
+
+if (string == 1){ 
+    print("Remember, remember, the 5th of November", quote=FALSE, file=stderr())
+    print("I know of no reason, why the gunpowder treason", quote=FALSE, file=stderr())
+    print("should ever be forgot", quote=FALSE, file=stderr())
+}
+
